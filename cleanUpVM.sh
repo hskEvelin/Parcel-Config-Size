ed7de24cb65eb1ebd06666853ef4daba2317a4e2
hskEvelin <lucas.greising@hs-kempten.de>
2018-01-26 09:43:27 +0100

    changed to vagrant

---
+++ cleanUpVM.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+vagrant destroy
+
+#stopping all containers, removing them and removing all images
+#ssh -p 3022 vm-uat@127.0.0.1 'docker-compose stop -d | docker rm $(docker ps -a -q) | docker rmi $(docker images -q)'
+
+#deleting all transfered files
+#ssh -p 3302 vm-uat@127.0.0.1 'rm parcelconfig-main parcelconfig-size parcel-size.component.js docker-compose.yml'
