b90e54850c580282aa2ce518ac2f11b60fb82311
hskEvelin <lucas.greising@hs-kempten.de>
2018-01-26 08:24:49 +0100

    changed script to vagrant VM

--- installDockerEnv.sh
+++ installDockerEnv.sh
@@ -5,15 +5,16 @@ do
 	docker save -o $var $var
 
 	#transfer tar file to remote machine via sftp on Port 3022
-	sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put '$var
+	#sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put '$var
 
 	#ssh command to load packed docker image in registry on remote machine
-	ssh -p 3022 vm-uat@127.0.0.1 'docker load -i '$var
+	vagrant ssh -c 'docker load -i /vagrant/'$var
 done
 
 #copy docker-compose file to remote machine
-sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put docker-compose.yml'
+#sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put docker-compose.yml'
+vagrant shh -c 'cp /vagrant/docker-compose.yml ~/'
 
 #start docker containers on remote machine
-ssh -p 3022 vm-uat@127.0.0.1 'docker-compose up -d'
+vagrant ssh c- 'docker-compose up -d'
 
