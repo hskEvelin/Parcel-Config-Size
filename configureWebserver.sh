1cb07e21455e7fc02e57469dc6d486e112bb7864
hskEvelin <lucas.greising@hs-kempten.de>
2018-01-26 09:37:08 +0100

    changed container name

--- configureWebserver.sh
+++ configureWebserver.sh
@@ -5,5 +5,5 @@ sleep 5s
 
 #copy .js file into docker container of webserver
 #ssh -p 3022 vm-uat@127.0.0.1 'docker cp parcel-size.component.js vmuat_webserver_1:/usr/local/tomcat/webapps/ParcelConfigService/js'
-vagrant ssh -c 'docker cp /vagrant/web/js/parcel-size.component.js vmuat_webserver_1:/usr/local/tomcat/webapps/ParcelConfigService/js'
+vagrant ssh -c 'docker cp /vagrant/web/js/parcel-size.component.js vagrant_webserver_1:/usr/local/tomcat/webapps/ParcelConfigService/js'
 	
