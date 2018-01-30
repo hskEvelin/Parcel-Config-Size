349f6364c6862d3903eeae79faf244727ac7f1f1
hskEvelin <lucas.greising@hs-kempten.de>
2018-01-30 15:41:47 +0100

    added deployment script

--- deployService.sh
+++ deployService.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 #transfer docker image to production vm
-#sftp -oPort=2222 vm-production@127.0.0.1 <<< $'put parcelconfig-size images/'
-
-#transfer js-files to production vm
-#sftp -oPort=2222 vm-production@127.0.0.1 <<< $'put web/js/parcel-size.component.js js/'
+sftp -oPort=2222 vm-production@127.0.0.1 <<< $'put parcelconfig-size images/'
 
 #load docker image to local docker registry and start container
-#ssh -p 2222 vm-production@127.0.0.1 'docker load -i parcelconfig-size | docker run -d -p 1121:1100 parcelconfig-size'
-
-result=$(ssh -p 2222 vm-production@127.0.0.1 'docker ps --filter ancestor=parcelconfig-main --format "{{.Names}}"')
+ssh -p 2222 vm-production@127.0.0.1 'docker load -i parcelconfig-size | docker run -d -p 1121:1100 parcelconfig-size'
 
 #load js file to webserver
-for i in $result
-do
-	echo $i
-	#ssh -p 2222 vm-production@127.0.0.1 'docker cp js/parcel-size.component.js $i:/usr/local/tomcat/webapps/ParcelConfigService/js'
-done
+sftp -oPort=2222 vm-production@127.0.0.1 <<< $'put web/js/parcel-size.component.js repository/js/'
+
+#change load balance config
+sftp -oPort=2222 vm-production@127.0.0.1 <<< $'put haproxy.cfg /etc/haproxy/'
+ssh -p 2222 vm-production@127.0.0.1 'sudo /etc/init.d/haproxy reload'
--- web/js/parcel-size.component.js
+++ web/js/parcel-size.component.js
@@ -21,7 +21,7 @@ angular.
 		  '<input class="w3-input" id="cfg-size-depth" type="text" ng-model="$ctrl.parcel.depth">'+
 		  '</form>'+
 		  '<br />'+
-		  '<button class="w3-btn w3-teal" ng-click="$ctrl.calcSize($ctrl.parcel)">Größe berechnen</button><label id="cfg-size-res">Paketgröße: {{$ctrl.parcel.size}}</label> '+
+		  '<button class="w3-btn w3-cyan" ng-click="$ctrl.calcSize($ctrl.parcel)">Größe berechnen</button><label id="cfg-size-res">Paketgröße: {{$ctrl.parcel.size}}</label> '+
 		  '</div>' ,
     	
     	
