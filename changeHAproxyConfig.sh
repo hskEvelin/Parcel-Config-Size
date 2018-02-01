553916512217fbcb6bb98be549d5d72bac615c21
hskEvelin <lucas.greising@hs-kempten.de>
2018-02-01 12:18:12 +0100

    switched production vm to vagrant

---
+++ changeHAproxyConfig.sh
@@ -0,0 +1,3 @@
+#change load balance config
+sftp -oPort=2223 vagrant@127.0.0.1 <<< $'put haproxy.cfg /etc/haproxy/'
+ssh -p 2223 vagrant@127.0.0.1 'sudo /etc/init.d/haproxy reload'
--- deployService.sh
+++ deployService.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 #transfer docker image to production vm
-sftp -oPort=2223 vm-production@127.0.0.1 <<< $'put parcelconfig-size images/'
+sftp -oPort=2223 vagrant@127.0.0.1 <<< $'put parcelconfig-size images/'
 
 #load docker image to local docker registry and start container
-ssh -p 2223 vm-production@127.0.0.1 'docker load -i images/parcelconfig-size'
+ssh -p 2223 vagrant@127.0.0.1 'docker load -i images/parcelconfig-size'
 
 result=$(ssh -p 2222 vm-production@127.0.0.1 'docker ps --filter ancestor=parcelconfig-size --format "{{.Names}}"')
 var=1
 for i in $result
 do
-	port=$(expr 1120 + $var)
-	ssh -p 2223 vm-production@127.0.0.1 'docker run -d -p $port:1100 parcelconfig-size'
+	port=$(expr 1100 + $var)
+	ssh -p 2223 vagrant@127.0.0.1 'docker run -d -p $port:1100 parcelconfig-size'
 	var=$((var+1))
 done	 
 
 #load js file to webserver
-sftp -oPort=2223 vm-production@127.0.0.1 <<< $'put web/js/parcel-size.component.js repository/js/'
+sftp -oPort=2223 vagrant@127.0.0.1 <<< $'put web/js/parcel-size.component.js repository/js/'
 
-#change load balance config
-sftp -oPort=2223 vm-production@127.0.0.1 <<< $'put haproxy.cfg /etc/haproxy/'
-ssh -p 2223 vm-production@127.0.0.1 'sudo /etc/init.d/haproxy reload'
+
