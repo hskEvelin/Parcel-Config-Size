3aa335a55b3398de0be37f75d1d878570d2f546a
hskEvelin <lucas.greising@hs-kempten.de>
2018-02-02 12:06:36 +0100

    added docker tag

--- installDockerEnv.sh
+++ installDockerEnv.sh
@@ -2,9 +2,22 @@
 #create VM and start it
 vagrant up --provider=virtualbox
 
+#while [ $# -gt 1 ]
+#do
+	#save docker image as tar file, so we can transfer it to remote machine
+	#docker save -o $1 $1
+
+	#transfer tar file to remote machine via sftp on Port 3022
+	#sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put '$var
+
+	#ssh command to load packed docker image in registry on remote machine
+	#docker load -i $1
+	#shift
+#done
+
 for var in "$@"
 do
-    	#save docker image as tar file, so we can transfer it to remote machine
+   	#save docker image as tar file, so we can transfer it to remote machine
 	docker save -o $var $var
 
 	#transfer tar file to remote machine via sftp on Port 3022
@@ -12,12 +25,18 @@ do
 
 	#ssh command to load packed docker image in registry on remote machine
 	vagrant ssh -c 'docker load -i /vagrant/'$var
+
+	#start docker containers
+	vagrant ssh -c 'docker run -d -p 1100:1100 '$var
 done
+
+
+#vagrant ssh -c 'docker run -d -p 1150:8080 parcelconfig-main'
 
 #copy docker-compose file to remote machine
 #sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put docker-compose.yml'
-vagrant ssh -c 'cp /vagrant/docker-compose.yml ~/'
+#vagrant ssh -c 'cp /vagrant/docker-compose.yml ~/'
 
 #start docker containers on remote machine
-vagrant ssh -c 'docker-compose up -d'
+#vagrant ssh -c 'docker-compose up -d'
 
