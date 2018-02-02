22cfb85d590859286d7fe6d0bb437c2da3380b4d
hskEvelin <lucas.greising@hs-kempten.de>
2018-02-02 12:34:44 +0100

    change version number of docker images

--- deployService.sh
+++ deployService.sh
@@ -3,6 +3,14 @@
 #transfer docker image to production vm
 sftp -oPort=2223 vagrant@127.0.0.1 <<< $'put parcelconfig-size images/'
 
+#add version number to old images
+result=$(ssh -p 2223 vagrant@127.0.0.1 'docker ps --filter ancestor=parcelconfig-size --format "{{.ID}}:{{.Names}}"')
+for j in $result
+do
+	arrImg=(${j//:/ })
+	ssh -p 2223 vagrant@127.0.0.1 'docker tag $arrImg[0] arrImg[1]:$1'
+done
+
 #load docker image to local docker registry and start container
 ssh -p 2223 vagrant@127.0.0.1 'docker load -i images/parcelconfig-size'
 
--- installDockerEnv.sh
+++ installDockerEnv.sh
@@ -2,17 +2,21 @@
 #create VM and start it
 vagrant up --provider=virtualbox
 
-#while [ $# -gt 1 ]
+#while [ $# -gt 0 ]
 #do
+#	img = $1
 	#save docker image as tar file, so we can transfer it to remote machine
-	#docker save -o $1 $1
+#	docker save -o $img $img
 
 	#transfer tar file to remote machine via sftp on Port 3022
 	#sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put '$var
 
 	#ssh command to load packed docker image in registry on remote machine
-	#docker load -i $1
-	#shift
+#	vagrant ssh -c 'docker load -i /vagrant/'$img
+
+#	shift
+	#start docker containers
+#	vagrant ssh -c 'docker run -d -p 1100:1100 '$img
 #done
 
 for var in "$@"
@@ -27,16 +31,12 @@ do
 	vagrant ssh -c 'docker load -i /vagrant/'$var
 
 	#start docker containers
-	vagrant ssh -c 'docker run -d -p 1100:1100 '$var
+	#vagrant ssh -c 'docker run -d -p 1100:1100 '$var
 done
 
-
-#vagrant ssh -c 'docker run -d -p 1150:8080 parcelconfig-main'
-
 #copy docker-compose file to remote machine
-#sftp -oPort=3022 vm-uat@127.0.0.1 <<< $'put docker-compose.yml'
-#vagrant ssh -c 'cp /vagrant/docker-compose.yml ~/'
+vagrant ssh -c 'cp /vagrant/docker-compose.yml ~/'
 
 #start docker containers on remote machine
-#vagrant ssh -c 'docker-compose up -d'
+vagrant ssh -c 'docker-compose up -d'
 
