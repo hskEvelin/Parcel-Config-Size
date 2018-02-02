ad2365597807f8b27208f1e5b8533319101941c8
hskEvelin <lucas.greising@hs-kempten.de>
2018-02-02 19:16:43 +0100

    uncomments

--- deployService.sh
+++ deployService.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #tag image with version number
-#docker tag $(docker images --filter=reference=parcelconfig-size:latest --format "{{.ID}}") parcelconfig-size:$1
-#docker save -o parcelconfig-size parcelconfig-size:$1
+docker tag $(docker images --filter=reference=parcelconfig-size:latest --format "{{.ID}}") parcelconfig-size:$1
+docker save -o parcelconfig-size parcelconfig-size:$1
 
 #transfer docker image to production vm
-#sftp -oPort=2223 vagrant@127.0.0.1 <<< $'put parcelconfig-size images/'
+sftp -oPort=2223 vagrant@127.0.0.1 <<< $'put parcelconfig-size images/'
 
 #add version number to old images
 #result=$(ssh -p 2223 vagrant@127.0.0.1 'docker ps --filter ancestor=parcelconfig-size --format "{{.ID}}:{{.Names}}"')
@@ -17,7 +17,7 @@
 #done
 
 #load docker image to local docker registry and start container
-#ssh -p 2223 vagrant@127.0.0.1 'docker load -i images/parcelconfig-size'
+ssh -p 2223 vagrant@127.0.0.1 'docker load -i images/parcelconfig-size'
 
 old_tag=$(($1 - 1))
 sshcmd='docker ps --filter ancestor=parcelconfig-size:'$old_tag' --format "{{.Names}}"'
@@ -33,6 +33,6 @@ do
 done	 
 
 #load js file to webserver
-#sftp -oPort=2200 vagrant@127.0.0.1 <<< $'put web/js/parcel-size.component.js repository/js/'
+sftp -oPort=2200 vagrant@127.0.0.1 <<< $'put web/js/parcel-size.component.js repository/js/'
 
 
